533e15a0321eae9de5675bf093889fe505ced6f1
Gustavo Amezcua <GustavoA@innovision.local>
2021-10-01 12:51:43 -0700

    finishing home page

--- blocks/two_columns_2.php
+++ blocks/two_columns_2.php
@@ -1,13 +1,32 @@
+
 <div class="<?php echo $field['acf_fc_layout'].'_block'; ?>">
-    <div class="row">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
-            <div><?php echo $field['text']; ?></div>
-            <a href="<?php echo $field['button']['url']; ?>" target="<?php echo $field['button']['target']; ?>" class="button_1 float_right mt-4"><span></span><span><?php echo $field['button']['title']; ?></span></a>
-        </div>
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
-            <?php echo !empty($field['slider_shortcode'])? do_shortcode($field['slider_shortcode']) : ''; ?>
-        </div>
-    </div>      
-    <!-- <div class="container">
-    </div> -->
+    <div class="container">
+        <?php 
+            $images = $field['images'];
+            $image_guide = '
+                <div class="grid-item">
+                    <a href="%s" target="%s" title="%s">
+                        <img src="%s" alt="%s" />
+                    </a>
+                </div>
+            ';
+            $images_content = null;
+            foreach( $images as $image ){
+                $images_content .= sprintf(
+                    $image_guide, 
+                    !empty($image['image_link']) ? $image['image_link']['url'] : '',
+                    !empty($image['image_link']) ? $image['image_link']['target'] : '',
+                    !empty($image['image_link']) ? $image['image_link']['title'] : '',
+                    !empty($image['image']) ? $image['image']['url'] : '',
+                    !empty($image['image']) ? $image['image']['alt'] : ''
+                );
+            }
+            echo !empty($field['title']) ? '<h2 class="text-align-center title-01">'.$field['title'].'</h2>' : '';
+            echo '
+                <div class="images_container masonry-grid">
+                    '.$images_content.'
+                </div>
+            ';
+        ?>
+    </div>
 </div>
