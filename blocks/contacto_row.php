c45f43cee5a1f07b414383d5e9d5bc3fedf3885f
Gustavo <gusta1296@gmail.com>
2021-12-05 18:49:56 -0800

    finished Contacto page

--- blocks/contacto_row.php
+++ blocks/contacto_row.php
@@ -1,11 +1,11 @@
 <div class="<?php echo $field['acf_fc_layout']; ?>_block">
     <div class="container"> 
         <div class="row">
-            <div class="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
                 <?php get_content($field['text']); ?>
                 <?php get_content($field['map_embed']); ?>
             </div>
-            <div class="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
                 <?php echo !empty($field['form_shortcode']) ? do_shortcode($field['form_shortcode']) : ''; ?>
             </div>
         </div>
